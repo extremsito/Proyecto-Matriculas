945d25214bb660bd47d551440d6d1d4a85e559c3
extremsito <134442620+extremsito@users.noreply.github.com>
2025-04-11 16:40:07 +0200

    Delete matriculas-project directory

--- matriculas-project/session.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION["usuario"])) {
-    echo json_encode(["loggedIn" => false]);
-    exit;
-}
-echo json_encode(["loggedIn" => true, "usuario" => $_SESSION["usuario"]]);
-?>
